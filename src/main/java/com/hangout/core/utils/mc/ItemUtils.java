becfec3b59930c36e4b5a73a387907921a595dbb
Randy Schouten <hydro_gamer@hotmail.com>
2015-07-30 14:42:34 +0200

    Fixed skull not showing skin

--- src/main/java/com/hangout/core/utils/mc/ItemUtils.java
+++ src/main/java/com/hangout/core/utils/mc/ItemUtils.java
@@ -3,6 +3,7 @@ package com.hangout.core.utils.mc;
 import java.util.List;
 
 import org.bukkit.Material;
+import org.bukkit.SkullType;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -20,7 +21,7 @@ public class ItemUtils {
 	}
 	
 	public static ItemStack getPlayerHead(Player p, String name, List<String> description){
-		ItemStack head = new ItemStack(Material.SKULL_ITEM);
+		ItemStack head = new ItemStack(Material.SKULL_ITEM, 1, (short) SkullType.PLAYER.ordinal());
 		SkullMeta im = (SkullMeta) head.getItemMeta();
         im.setOwner(p.getName());
         im.setDisplayName(name);
